d45a42a73e739c168e88d9952403d43fdf690797
Andreas Sjödin <andreas.sjodin@gmail.com>
2019-06-12 21:15:52 +0200

    Update setup.py
    * Bump version to 1.1.0
    * Skip support for py27
    * Increase status to beta

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ def readme():
 
 setup(
     name='doepipeline',
-    version='1.0.1',
+    version='1.1.0',
     description='Package for optimizing pipelines using DoE.',
     long_description=readme(),
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering'
     ],
     keywords='pipeline doe optimization',
